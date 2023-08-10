39198f3597d1c92c4ea29817039fc50f2886e1a5
Benoit Dewigne <benoitdewigne@gmail.com>
2023-08-10 16:25:57 +0200

    add contact page

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,6 +5,7 @@ import NavBar from './components/NavBar';
 import Footer from './components/Footer';
 import HomePage from './components/views/HomePage';
 import ServicesPage from './components/views/ServicesPage';
+import ContactPage from './components/views/ContactPage';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/services" element={<ServicesPage />} />
+          <Route path="/contact" element={<ContactPage /> } />
         </Routes>
       </div>
       <Footer />
